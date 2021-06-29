ddfd8afc79c474c6b04c60a360b2a1a65036b2a5
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-06-29 09:41:12 +0500

    Added virtual env variable

--- flask/config.py
+++ flask/config.py
@@ -1,8 +1,14 @@
+import os
+
+USER_DB = os.getenv('USER_DB')
+NAME_DB = os.getenv('NAME_DB')
+PASSWORD_DB = os.getenv('PASSWORD_DB')
+
 CONFIG = {
     'DEBUG': True,
     'ENV': 'development',
-    'POSTGRES_URI': 'postgresql://postgres:748429itd@127.0.0.1:5431/advertisements_db',
-    'SQLALCHEMY_DATABASE_URI': 'postgresql://postgres:748429itd@127.0.0.1/advertisements_db',
+    'POSTGRES_URI': f"postgresql://{USER_DB}:{PASSWORD_DB}@127.0.0.1:5431/{NAME_DB}",
+    'SQLALCHEMY_DATABASE_URI': f'postgresql://{USER_DB}:{PASSWORD_DB}@127.0.0.1/{NAME_DB}',
     'SALT': 'my_sJHLHLHKLаваыпuper_s!alt_#4$4344',
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
     'JWT_SECRET_KEY': 'jwt-secret-string',
