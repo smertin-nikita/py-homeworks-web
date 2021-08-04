4ee25fd120147a8203da8d3585ada95c101dfc98
Smertin Nikita <smertin-nikita@mail.ru>
2021-08-04 19:31:52 +0500

    get persons

--- asyncio/main.py
+++ asyncio/main.py
@@ -1,18 +1,27 @@
 import asyncio
+from pprint import pprint
 
 import aiohttp as aiohttp
 
+LINK = "https://swapi.dev/api/people/"
 
-async def get_person(url: str):
+
+async def make_request(url: str):
     async with aiohttp.client.ClientSession() as session:
         async with session.get(url) as response:
-            json = await response.json()
-            return json
+            return await response.json()
+
+
+async def get_persons(count: int = 25):
+    tasks = [asyncio.create_task(make_request(f'{LINK}{count}')) for _ in range(1, count + 1)]
+    return await asyncio.gather(*tasks)
 
 
 async def main():
-    persons = get_person()
+    persons = await get_persons()
+    pprint(persons)
+
 
 if __name__ == '__main__':
-    # asyncio.get_event_loop().run_until_complete(main())
-    asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
+    # asyncio.run(main())
